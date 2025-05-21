3583879b87ae9c2b7c9fe0f1aa5af704076cdece
Skulletor666 <aburto@gmail.com>
2025-05-20 23:56:57 -0600

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>Gather Clone Activo</h1>
+      <p>¡Tu cowork virtual está en línea!</p>
+    </div>
+  );
+}
